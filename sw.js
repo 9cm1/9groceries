295d9d298ab8bd0556d1b82e39766cd755a30316
9cm1 <micheloconrad@gmail.com>
2025-09-27 17:47:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,28 @@
 const CACHE_NAME = '9groceries-v1';
 const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json',
-    './icons/icon-192.png',
-    './icons/icon-512.png'
+  '/9groceries/',
+  '/9groceries/index.html',
+  '/9groceries/manifest.json',
+  '/9groceries/icons/icon-192.png',
+  '/9groceries/icons/icon-512.png',
+  '/9groceries/sw.js'
 ];
 
 // Install event - cache files
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(urlsToCache))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 // Fetch event - serve from cache
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-        .then(response => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
 });
